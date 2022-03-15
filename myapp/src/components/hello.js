de9b995f2df10e938e786422c8709af8cd8a8bee
Ahadbek5092 <alijandanwh3@gmail.com>
2022-03-15 10:59:05 +0500

    rest

--- myapp/src/components/hello.js
+++ myapp/src/components/hello.js
@@ -1,6 +1,6 @@
 import React from "react";
 function hello() {
-  return <div>hello world I am learning React</div>;
+  return <div>hello world I am learning React and Django </div>;
 }
 
 export default hello;
